6dcf48ebb781e67e6444e9c9e48d52683b170c51
Zeph Ng <zeph1997@hotmail.com>
2017-09-02 16:12:11 +0000

    Added server side code for creating subs for pro users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   belongs_to :plan
+  
+  attr_accessor :stripe_card_token
+  def save_with_subscription
+    if valid?
+      #these keys are actually predefined by Stripe and can be found in their documentation
+      customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
+      self.stripe_customer_token = customer.id
+      save!
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to:'pages#home'
+  devise_for :users, controllers: {registrations: 'users/registrations' }
   get 'about', to: 'pages#about'
   resources :contacts, only: [:create]
   get 'contact-us', to: 'contacts#new', as: 'new_contact'
